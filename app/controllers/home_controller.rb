058071ae49b8e420a88fa98bb8bd9567fb09c7ae
MCDD <jessestarr2@naver.com>
2016-07-11 07:55:36 +0000

    jy0455

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,30 @@
 class HomeController < ApplicationController
   def index
+    @every_allcreator = Allcreator.all
   end
   
   def all_creator
+    @every_allcreator = Allcreator.all
   end
   
   def mypage
+     
   end
   
   def url_upload
+    
+    
+    @creatorname = params[:name]
+    @creatortitle = params[:title]
+    @creatorurl = params[:url]
+    
+    new_allcreator =Allcreator.new
+    new_allcreator.name = @creatorname
+    new_allcreator.title = @creatortitle
+    new_allcreator.url = @creatorurl
+    new_allcreator.save
+    
+    redirect_to "/home/all_creator"
   end
   
 end
